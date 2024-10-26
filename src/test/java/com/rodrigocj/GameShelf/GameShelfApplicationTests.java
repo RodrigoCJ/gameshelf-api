8feb9f5d21b562745d796b5f175f913a0b6470d5
Rodrigo <rdrgcornejo@hotmail.com>
2024-10-26 15:18:28 -0300

    first commit

---
+++ src/test/java/com/rodrigocj/GameShelf/GameShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocj.GameShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
